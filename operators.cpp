1fa0d071f20c62a3edc296bbca1bfe898e13fcc9
Shagun <shagunmi357@gmail.com>
2026-06-06 13:12:30 +0530

    Operator

---
+++ operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int a;
+   int b ;
+   cout << "Enter values of a and b:";
+   cin >> a>> b;
+   cout << "a + b = "<< a + b << endl;
+   cout << "a - b = "<< a - b << endl;
+   cout << "a * b = "<< a * b << endl;
+   cout << "a / b = "<< a / b << endl;
+   cout << "a % b = "<< a % b << endl;
+   return 0;
+
+}
