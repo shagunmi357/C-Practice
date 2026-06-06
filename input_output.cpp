d91d78e272316295474c4a116a49ec14d1d7e0bc
Shagun <shagunmi357@gmail.com>
2026-06-06 12:57:03 +0530

    input output

---
+++ input_output.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    float percentage ;
+    cout << "Enter value of a :";
+    cin>> a;
+    cout << "Enter Precentage :";
+    cin >> percentage;
+    cout << "The value of a is: " << a << endl;
+    cout << "The percentage is: " << percentage << endl;
+    return 0;
+}
