7489b0fd100b83a6cf6e8cd146c5c165216c808f
Shagun <shagunmi357@gmail.com>
2026-06-06 15:23:54 +0530

    Unary operators

---
+++ unary_operator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // pre increment operator
+    int a = 10;
+    ++a; // increments a by 1 before using its value
+    cout << a << endl;
+    //post increment operator 
+    int b = 20;
+    b++;
+    cout << b <<endl;
+    int j = 10;
+    --j; // decrements j by 1 before using its value
+    cout << j << endl;
+    //post decrement operator 
+    int k = 30;
+    k--;
+    cout << k <<endl;
+    
+    return 0;
+
+}
