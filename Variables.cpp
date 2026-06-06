dda6445ddc2e2f067f7bc2d474f541121c1d4802
Shagun <shagunmi357@gmail.com>
2026-06-06 12:30:21 +0530

    Variables

---
+++ Variables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    char Name[] = "john";
+    float b = 3.13;
+    double c = 3.1459867238;
+    cout << a << endl;
+    cout << Name << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
+}
