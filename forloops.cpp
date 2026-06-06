7edfcf89282c3b042473c5cfe62de826e8ffe524
Shagun <shagunmi357@gmail.com>
2026-06-06 14:55:39 +0530

    while loops

--- forloops.cpp
+++ forloops.cpp
@@ -4,5 +4,9 @@ int main(){
     for (int i = 1; i <= 10; i = i + 1){
         cout << "7 *"<< i << "="<< 7*i << endl;
         }
+    for(int i = 101; i>= 1; i= i-1){
+        cout << i << endl;
+        
+    }
         return 0;
     }
---
+++ while_loops.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char c = 'a';
+    while(c <= 'z'){
+        cout << c << endl;
+        c = c + 1;
+    }
+    return 0;
+}
