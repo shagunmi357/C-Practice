c1341b2a49950a806f97312c5547fcf8d48b8a49
Shagun <shagunmi357@gmail.com>
2026-06-07 17:16:58 +0530

    function

---
+++ functions.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int sum(int a , int b){
+    return a+b;
+
+}
+int main(){
+    int x = 4;
+    int y = 9;
+    cout << sum(x, y) << endl;
+    return 0;
+}
