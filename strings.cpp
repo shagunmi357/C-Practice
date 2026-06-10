2786275dcc9f8e0a78d2578117265c8d9f2ed63e
Shagun <shagunmi357@gmail.com>
2026-06-10 14:42:42 +0530

    Strings

---
+++ strings.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string Name;
+    Name.push_back('R');
+    Name.push_back('o');
+    Name.push_back('b');
+    Name.push_back('e');
+    Name.push_back('r');
+    Name.push_back('t');
+    cout << "Name: " << Name << endl;
+    return 0;
+}
